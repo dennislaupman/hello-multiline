d23b5d9346515786d6972a2b882618748b503241
Dennis Laupman <dennis@pluxbox.com>
2015-12-19 19:41:02 +0100

    added basics

---
+++ hello-server.js
@@ -0,0 +1,62 @@
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/hello-multiline.html');
+
+
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+var connectedClients = [];
+
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    
+    //Add to clinets pool
+    connectedClients.push( { socketId: socket.id } );
+
+    io.emit('connectedClients', { connectedClients: connectedClients  });
+
+    socket.emit('getSocketId', { socketId: socket.id  }  )
+
+    socket.on('setSelectionRange', function( data ) {
+
+        console.log(data)
+    	socket.broadcast.emit('getSelectionRange', data);
+    });
+
+    socket.on('disconnect', function ( ) {
+
+        socket.broadcast.emit('removeClient', socket.id);
+
+        console.log('user disconnected',   socket.id);
+
+        for (var i = connectedClients.length - 1; i >= 0; i--) {
+            
+            if( connectedClients[i].socketId === socket.id) {
+
+                connectedClients.splice(i, 1);
+
+
+
+                io.emit('connectedClients', { connectedClients: connectedClients  });
+                break;
+            }
+        };
+  });
+});
+
+
+
+
+app.listen(3000);
+
